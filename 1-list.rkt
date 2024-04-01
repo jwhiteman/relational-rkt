3eace34a397742ffff076f40db60422961500313
Jim Whiteman <jimtron9000@gmail.com>
2024-04-01 17:38:40 -0600

    continued

--- 1-list.rkt
+++ 1-list.rkt
@@ -1,5 +1,53 @@
+;; simplifications:
+;; reify
+;; append
+;; append-map ?
+
+(define succeed
+  (lambda (s)
+    `(,s)))
+
+(define (conj2 g1 g2)
+  (lambda (s)
+    (append-map-inf g2 (g1 s))))
+
+;; (append-map vector->list '(#(1) #(2 3) #(4)))
+(define (append-map-inf g s-inf)
+  (cond
+    ((null? s-inf) '())
+    ((pair? s-inf)
+     (append-inf (g (car s-inf))
+                 (append-map-inf g (cdr s-inf))))
+    (else
+      (lambda ()
+        (append-map-inf g (s-inf))))))
+
+(define (append-inf s-inf t-inf)
+  (cond
+    ((null? s-inf) t-inf)
+    ((pair? s-inf)
+     (cons (car s-inf)
+           (append-inf (cdr s-inf) t-inf)))
+    (else
+      (lambda ()
+        (append-inf t-inf (s-inf))))))
+
+;; 1.
 (run* q
   (conj2 succeed succeed))
+
+(let ((q (var 'q)))
+  (run-goal #f
+            (conj2 succeed succeed)))
+
+;; 1. reify is per substitution (probably via map)
+;; 2. is done against a single variable [question: how does (run (q b ..) ...) work?]
+;; 3. terminal meaning -> +for contained vars -> replace fresh vars w/ numbers
+;;    note: _0 appears in a list here because we're mapping
+(let ((q (var 'q)))
+  (map (reify q)
+       (run-goal #f
+                 (conj2 succeed succeed))))
 
 (run* q
   (conj2 succeed (== 'corn q)))
