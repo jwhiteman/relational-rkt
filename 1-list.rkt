bf4aa47739b76481a93e4f97877feab2df160219
Jim Whiteman <jimtron9000@gmail.com>
2024-04-02 10:31:42 -0600

    continued

--- 1-list.rkt
+++ 1-list.rkt
@@ -1,8 +1,3 @@
-;; simplifications:
-;; reify
-;; append
-;; append-map ?
-
 (define succeed
   (lambda (s)
     `(,s)))
@@ -49,53 +44,115 @@
        (run-goal #f
                  (conj2 succeed succeed))))
 
+;; 2.
 (run* q
   (conj2 succeed (== 'corn q)))
 
+(let ((q (var 'q)))
+  (map (reify q) (run-goal #f (conj2
+                                succeed
+                                (== 'corn q)))))
+
+;; 3
+;; fail => empty stream
+;; append-map(empty-stream => '()
+;; (reify '() q)           => '()
 (run* q
   (conj2 fail (== 'corn q)))
 
+;; 4
+;; l evaluates to the stream w/ single substitution: '((,q . corn))
+;; is mapped over, first s being (,q . corn)
+;; ...which causes the map function/goal (== 'mail q) to fail
+;; (append '()) => '()
 (run* q
   (conj2 (== 'corn q) (== 'meal q)))
 
+;; 5
+;; append-map:
+;; stream is ( ((,q . corn)) )
+;; g is the unifictation (== 'corn q)
+;; g is applied to the single substittion - unification succeeds -
+;; output: '((,q . corn))
+;; |> reify w/ respect to q:'(corn)
 (run* q
   (conj2 (== 'corn q) (== 'corn q)))
 
+;; 6
+;; append '() '() => '()
 (run* q
   (disj2 fail fail))
 
+;; 7
+;; append ( ((,q . olive)) ) () => (((,q . olive)))
+;; map over stream using (reify q) => '(olive)
+;; note: reify uses walk*/reify-s/walk*
 (run* q
   (disj2 (== 'olive q) fail))
 
+;; 8
+;; append () ({q: oil}) => ({q: oil})
+;; => '(oil)
 (run* q
   (disj2 fail (== 'oil q)))
 
+;; 9
+;; append ({q: olive}) ({q: oil}) => ({q: olive}, {q: oil})
+;; then map both substitutions, via reify => (olive oil)
 (run* q
   (disj2 (== 'olive q) (== 'oil q)))
 
+;; 10
+;; append ({q: ($x $y)}) ({q: ($y, $x)) => ( {q: ($x $y)}, {q: ($y, $x)} )
+;; map w/ (reify q) => ((0 1) (0 1))
 (run* q (fresh (x)
           (fresh (y) (disj2
                        (== `(,x ,y) q)
                        (== `(,y ,x) q)))))
 
+;; 11
+;; append ({x: olive}) ({x: oil}) => ({x: olive}, {x: oil})
+;; map w/ (reify x) => (olive oil)
 (run* x
   (disj2 (== 'olive x) (== 'oil x)))
 
+;; 12 - ditto, w/ result in different order
 (run* x
   (disj2 (== 'oil x) (== 'olive x)))
 
+;; 13
+;; append
+;;   1: append-map ({x: olive}), fail => '()
+;;   2. ({x: oil})
+;; append (), ({x: oil}) => ({x: oil})
+;; map w/ (reify x) '(oil)
 (run* x
   (disj2
     (conj2 (== 'olive x) fail) (== 'oil x)))
 
+;; 14
+;; append
+;;   1. append-map ({x: olive}), succeed =>  ({x: olive})
+;;   2. ({x: oil})
+;; append ({x: olive}) ({x: oil}) => ({x: olive}, {x: oil})
+;; map w/ (reify x) => '(olive oil)
 (run* x
   (disj2
     (conj2 (== 'olive x) succeed) (== 'oil x)))
 
+;; 15 - ditto, but w/ result in different order - (oil olive)
 (run* x (disj2
           (== 'oil x)
           (conj2 (== 'olive x) succeed)))
 
+;; 16
+;; append
+;;   - append-map: ({x: virgin}), fail           => ()
+;;   - append:                                   => ({x: olive}, {}, {x: oil})
+;;     - ({x: olive})
+;;     - append: (()) ({x: oil})                 => ({}, {x: oil})
+;; append (), ({x: olive}, {}, {x: oil})         => ({x: olive}, {}, {x: oil})
+;; map w/ (reify x)                              => (olive _0 oil)
 (run* x
   (disj2
     (conj2 (== 'virgin x) fail)
@@ -104,6 +161,7 @@
                        succeed
                        (== 'oil x)))))
 
+;; 17
 (run* r
   (fresh (x)
     (fresh (y) (conj2
@@ -111,18 +169,20 @@
                                   (== 'pea y)
                                   (== `(,x ,y) r))))))
 
+;; 18
 (run* r
   (fresh (x)
     (fresh (y) (conj2
-                 (conj2
-                   (== 'split x)
-                   (== 'pea y)) (== `(,x ,y) r)))))
+                 (conj2 (== 'split x) (== 'pea y))
+                 (== `(,x ,y) r)))))
 
-(run* (x y)
+;; 19
+(run* (x y) ;; which macro is this using?
   (disj2
     (conj2 (== 'split x) (== 'pea y))
     (conj2 (== 'red x) (== 'bean y))))
 
+;; 20
 (run* r
   (fresh (x y)
     (conj2 (disj2
